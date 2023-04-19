3cb4dd863d902809850a9bf0b48309eccae4a005
reillykeele <54957139+reillykeele@users.noreply.github.com>
2023-04-19 00:03:47 -0500

    Add Modal, Default Font

---
+++ Template/Assets/_Scripts/UI/ButtonControllers/DisplayPopupModalButtonController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Util.Attributes;
+using Util.Helpers;
+using Util.UI.Controllers.Selectables.Buttons;
+using Util.UI.Modals;
+
+namespace Template.UI.ButtonControllers
+{
+    public class DisplayPopupModalButtonController : AButtonController
+    {
+        [SerializeField] [PrefabOnly] private ModalController _modalPrefab;
+
+        [SerializeField] private string _modalTitle = "";
+        [SerializeField] private string _modalDescription = "";
+
+        public UnityEvent OnModalYesEvent = new UnityEvent();
+        public UnityEvent OnModalNoEvent = new UnityEvent();
+
+        private ModalController _modal;
+
+        void Start()
+        {
+            _modal = Instantiate(_modalPrefab, _canvasController.transform);
+            _modal.gameObject.Disable();
+        }
+
+        public override void OnClick()
+        {
+            _modal.DisplayModal(_modalTitle, _modalDescription, OnModalYesEvent.Invoke, OnModalNoEvent.Invoke);
+        }
+
+    }
+}
